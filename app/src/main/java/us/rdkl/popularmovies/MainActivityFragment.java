e8e40c01e09b920db170fe1484d0375dc5f5f675
Russ Bernhardt <russell.bernhardt@gmail.com>
2015-12-13 14:00:35 -0800

    Finish adding debug refresh menu item

--- app/src/main/java/us/rdkl/popularmovies/MainActivityFragment.java
+++ app/src/main/java/us/rdkl/popularmovies/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -90,5 +93,28 @@ public class MainActivityFragment extends Fragment {
     public void onStart() {
         super.onStart();
         reloadMovies();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.movie, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            reloadMovies();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
